1a9c5939f43df10ae7e88e9b4be1b13e482c7de7
Delia Puspitasari <115056525+taehyungggg29@users.noreply.github.com>
2023-03-17 16:03:00 +0700

    Praltikum04

---
+++ Praktikum04/rumus.php
@@ -0,0 +1,17 @@
+<?php
+
+function luasLingkaran($jariJari)
+{
+        define('PHI', 3.15);
+        echo PHI *$jariJari *  $jariJari;
+}
+
+function luarPersegi($sisi)
+{
+    echo $sisi * $sisi;
+}
+
+function luarPersegi ($alas, $tinggi)
+{
+    echo 1/2 * $alas *$tinggi;
+}
